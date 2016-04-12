b340b4808b8944b0f927515b705fe9bc71aed33e
Tummala Krishna Kishore <krishna30kishore@gmail.com>
2016-04-12 15:38:32 +0530

    Ionic - iOS

---
+++ platforms/windows/www/scripts/services.js
@@ -0,0 +1,288 @@
+﻿
+angular.module('starter.services', [])
+.factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+    }];
+
+    return {
+        all: function () {
+            return chats;
+        },
+        remove: function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
+        }
+    };
+})
+.factory('Posts', function ($http) {
+    return {
+        getallusers: function () {
+            var url = "http://wcf.masterek.org/MasterEKService.svc/GetArchiveSubNews";
+            return $http.post(url, { objInput: { "LanguageID": 1 } });
+        }
+    };
+})
+//Narendra Starting
+ .factory('archiveimg', function ($http, $ionicPopup) {
+     //if (window.Connection) {
+     //    if (navigator.connection.type == Connection.NONE) {
+     //        $ionicPopup.confirm({
+     //            title: "Alert!",
+     //            content: "No Internet connection Available.Please check your connection and try again."
+     //        }).then(function (result) {
+     //            if (!result) {
+     //                ionic.Platform.exitApp();
+     //            }
+     //        });
+     //    }
+     //    else
+     //        if (navigator.connection.type != Connection.NONE) {
+                 return {
+                     GetArchiveImages: function () {
+                         var url = "http://wcf.masterek.org/MasterEKService.svc/GetArchiveImages";
+                         return $http.post(url);
+                     },
+                     GetLoadImages: function (ImageEventId) {
+
+                         var url = "http://wcf.masterek.org/MasterEKService.svc/GetLoadImages";
+                         return $http.post(url, { ObjInput: { "ImageEventId": ImageEventId } });
+                     }
+                 };
+     //        }
+     //}
+
+
+ })
+
+ .factory('getimgcontroller', function ($http, $ionicPopup) {
+
+     return {
+         GetImages: function () {
+             //if (window.Connection) {
+             //    if (navigator.connection.type == Connection.NONE) {
+             //        $ionicPopup.confirm({
+             //            title: "Alert!",
+             //            content: "No Internet connection Available.Please check your connection and try again."
+             //        }).then(function (result) {
+             //            if (!result) {
+             //                return ("No Internet connection Available.Please check your connection and try again.");
+             //            }
+             //        });
+             //    }
+             //    else {
+                    // if (navigator.connection.type != Connection.NONE) {
+                         var url = "http://wcf.masterek.org/MasterEKService.svc/GetImages";
+                         return $http.post(url);
+                    // }
+
+             //    }
+             //}
+
+         },
+         GetLoadImages: function (ImageEventId) {
+             //if (window.Connection) {
+             //    if (navigator.connection.type == Connection.NONE) {
+             //        $ionicPopup.confirm({
+             //            title: "Alert!",
+             //            content: "No Internet connection Available.Please check your connection and try again."
+             //        }).then(function (result) {
+             //            if (!result) {
+             //                return ("No Internet connection Available.Please check your connection and try again.");
+             //            }
+             //        });
+             //    }
+             //    else {
+                     //if (navigator.connection.type != Connection.NONE) {
+                          var url = "http://wcf.masterek.org/MasterEKService.svc/GetLoadImages";
+             return $http.post(url, { ObjInput: { "ImageEventId": ImageEventId } });
+                   //  }
+
+             //    }
+             //}
+          
+
+         }
+     };
+
+ })
+
+.factory('homeController', function ($http, $ionicPopup) {
+    //if (window.Connection) {
+    //    if (navigator.connection.type == Connection.NONE) {
+    //        $ionicPopup.confirm({
+    //            title: "Alert!",
+    //            content: "No Internet connection Available.Please check your connection and try again."
+    //        }).then(function (result) {
+    //            if (!result) {
+    //                ionic.Platform.exitApp();
+    //            }
+    //            else {
+    //                return ("No Internet connection Available.Please check your connection and try again.");
+    //            }
+    //        });
+    //    }
+    //    else
+    //        if (navigator.connection.type != Connection.NONE) {
+                return {
+                    GetArchiveSubNews: function () {
+                        var url = "http://wcf.masterek.org/MasterEKService.svc/GetArchiveSubNews";
+                        return $http.post(url, { objInput: { "LanguageID": 1 } });
+                    }
+                };
+    //        }
+    //}
+
+})
+
+.factory('cnewsController', function ($http, $ionicPopup) {
+    //if (window.Connection) {
+    //    if (navigator.connection.type == Connection.NONE) {
+
+    //        $ionicPopup.confirm({
+    //            title: "Alert!",
+    //            content: "No Internet connection Available.Please check your connection and try again."
+    //        }).then(function (result) {
+    //            if (!result) {
+    //                ionic.Platform.exitApp();
+    //            }
+    //        });
+
+
+    //    }
+    //    else
+            //if (navigator.connection.type != Connection.NONE) {
+                return {
+                    GetCurrentSubNews: function () {
+                        var url = "http://wcf.masterek.org/MasterEKService.svc/GetCurrentSubNews";
+                        return $http.post(url, { objInput: { "LanguageID": 1 } });
+                    }
+                };
+
+    //        }
+    //}
+
+})
+
+.factory('eventscontroller', function ($http, $ionicPopup) {
+    //if (window.Connection) {
+    //    if (navigator.connection.type == Connection.NONE) {
+    //        $ionicPopup.confirm({
+    //            title: "Alert!",
+    //            content: "No Internet connection Available.Please check your connection and try again."
+
+    //        }).then(function (result) {
+    //            if (!result) {
+    //                ionic.Platform.exitApp();
+    //            }
+    //        });
+    //    }
+    //    else
+    //        if (navigator.connection.type != Connection.NONE) {
+                return {
+                    DisplayEvents: function () {
+                        var url = "http://wcf.masterek.org/MasterEKService.svc/DisplayEvents";
+                        return $http.post(url);
+                    }
+                };
+    //        }
+
+    //}
+
+
+})
+
+
+
+
+.factory('getcaptioncontroller', function ($http) {
+    return {
+        GetCaptions: function () {
+
+            var url = "http://wcf.masterek.org/MasterEKService.svc/GetCaptions";
+            return $http.post(url);
+        }
+    };
+
+})
+
+
+.factory('marqueeimagescontroller', function ($http) {
+    return {
+        MarqueeImages: function () {
+
+            var url = "http://wcf.masterek.org/MasterEKService.svc/MarqueeImages";
+            return $http.post(url);
+        }
+    };
+
+})
+
+.service('ModalService', function ($ionicModal, $rootScope) {
+
+
+    var init = function (tpl, $scope) {
+
+        var promise;
+        $scope = $scope || $rootScope.$new();
+
+        promise = $ionicModal.fromTemplateUrl(tpl, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+            return modal;
+        });
+
+        $scope.openModal = function () {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
+        });
+
+        return promise;
+    }
+
+    return {
+        init: init
+    }
+
+})
+//ending;
---
+++ platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,191 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-hockeyapp/www/hockeyapp.js",
+        "id": "cordova-plugin-hockeyapp.HockeyApp",
+        "clobbers": [
+            "hockeyapp"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media/www/MediaError.js",
+        "id": "cordova-plugin-media.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media/www/Media.js",
+        "id": "cordova-plugin-media.Media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
+        "id": "org.apache.cordova.file.DirectoryEntry",
+        "clobbers": [
+            "window.DirectoryEntry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/DirectoryReader.js",
+        "id": "org.apache.cordova.file.DirectoryReader",
+        "clobbers": [
+            "window.DirectoryReader"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Entry.js",
+        "id": "org.apache.cordova.file.Entry",
+        "clobbers": [
+            "window.Entry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/File.js",
+        "id": "org.apache.cordova.file.File",
+        "clobbers": [
+            "window.File"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileEntry.js",
+        "id": "org.apache.cordova.file.FileEntry",
+        "clobbers": [
+            "window.FileEntry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileError.js",
+        "id": "org.apache.cordova.file.FileError",
+        "clobbers": [
+            "window.FileError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileReader.js",
+        "id": "org.apache.cordova.file.FileReader",
+        "clobbers": [
+            "window.FileReader"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileSystem.js",
+        "id": "org.apache.cordova.file.FileSystem",
+        "clobbers": [
+            "window.FileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileUploadOptions.js",
+        "id": "org.apache.cordova.file.FileUploadOptions",
+        "clobbers": [
+            "window.FileUploadOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileUploadResult.js",
+        "id": "org.apache.cordova.file.FileUploadResult",
+        "clobbers": [
+            "window.FileUploadResult"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileWriter.js",
+        "id": "org.apache.cordova.file.FileWriter",
+        "clobbers": [
+            "window.FileWriter"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Flags.js",
+        "id": "org.apache.cordova.file.Flags",
+        "clobbers": [
+            "window.Flags"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/LocalFileSystem.js",
+        "id": "org.apache.cordova.file.LocalFileSystem",
+        "clobbers": [
+            "window.LocalFileSystem"
+        ],
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Metadata.js",
+        "id": "org.apache.cordova.file.Metadata",
+        "clobbers": [
+            "window.Metadata"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/ProgressEvent.js",
+        "id": "org.apache.cordova.file.ProgressEvent",
+        "clobbers": [
+            "window.ProgressEvent"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/fileSystems.js",
+        "id": "org.apache.cordova.file.fileSystems"
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/requestFileSystem.js",
+        "id": "org.apache.cordova.file.requestFileSystem",
+        "clobbers": [
+            "window.requestFileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/resolveLocalFileSystemURI.js",
+        "id": "org.apache.cordova.file.resolveLocalFileSystemURI",
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/wp/FileUploadOptions.js",
+        "id": "org.apache.cordova.file.FileUploadOptions1",
+        "merges": [
+            "window.FileUploadOptions"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-hockeyapp": "2.1.0",
+    "cordova-plugin-media": "2.2.0",
+    "cordova-plugin-network-information": "1.2.0",
+    "cordova-plugin-splashscreen": "3.2.1",
+    "cordova-plugin-whitelist": "1.2.2-dev",
+    "org.apache.cordova.file": "1.3.3"
+}
+// BOTTOM OF METADATA
+});
